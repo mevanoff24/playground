b048a3947cb340297fa8914b92f9ca88f71d4495
Matt Evanoff <mevanoff24@gmail.com>
2015-03-24 12:39:15 -0700

    better quicksort

--- sort/quick_sort.rb
+++ sort/quick_sort.rb
@@ -1,32 +1,61 @@
 # In Place
 
-def quick_sort(array, min = 0, max = array.length - 1)
-	if min < max
-		index = partition(array, min, max)
-		quick_sort(array, min, index - 1)
-		quick_sort(array, index + 1, max)
-	end
-
-end
-
-def partition(array, left, right)
-	middle = (left + right) / 2
-	pivot = array[middle]
-	array[middle], array[right] = array[right], array[middle]
-
-	current = left
-
-  (left...right).each do |index|
-    if array[index] <= pivot
-      array[index], array[current] = array[current], array[index]
-      current += 1
+class QuickSort
+ 
+  def self.quick(keys, left = 0, right = keys.length-1)
+    if left < right
+      pivot = partition(keys, left, right)
+      quick(keys, left, pivot - 1)
+      quick(keys, pivot + 1, right)
     end
+    keys
   end
-	array[current], array[right] = array[right], array[current]
-	return current
+ 
+  def self.partition(keys, left, right)
+    x = keys[right]
+    index = left - 1
+    for number in left..right - 1
+      if keys[number] <= x
+        index += 1
+        keys[index], keys[number] = keys[number], keys[index]
+      end
+    end
+    keys[index+1], keys[right] = keys[right], keys[index+1]
+    index+1
+  end
+ 
 end
 
-p quick_sort([4,6,2,8,1])
+p QuickSort.quick([4,6,2,8,1])
+
+
+# def quick_sort(array, min = 0, max = array.length - 1)
+# 	if min < max
+# 		index = partition(array, min, max)
+# 		quick_sort(array, min, index - 1)
+# 		quick_sort(array, index + 1, max)
+# 	end
+
+# end
+
+# def partition(array, left, right)
+# 	middle = (left + right) / 2
+# 	pivot = array[middle]
+# 	array[middle], array[right] = array[right], array[middle]
+
+# 	current = left
+
+#   (left...right).each do |index|
+#     if array[index] <= pivot
+#       array[index], array[current] = array[current], array[index]
+#       current += 1
+#     end
+#   end
+# 	array[current], array[right] = array[right], array[current]
+# 	return current
+# end
+
+# p quick_sort([4,6,2,8,1])
 
 
 # Out of Place
