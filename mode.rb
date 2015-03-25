de6a0d89d63426728874d1cbef2501bc018f6752
Matt Evanoff <mevanoff24@gmail.com>
2015-03-24 19:56:59 -0700

    mode

---
+++ mode.rb
@@ -0,0 +1,19 @@
+
+
+def mode(array)
+	counter = Hash.new(0)
+	array.each do |index|
+		counter[index] +=1
+	end
+
+	result = []
+
+	counter.each do |key, value|
+		if value == counter.values.max 
+			result << key
+		end
+	end
+	result
+end
+
+p mode([1,2,3,3,4,6])
