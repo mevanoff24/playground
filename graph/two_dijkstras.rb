ae6dbb57a36bc7997ba6f305393c9db6c2814360
Matt Evanoff <mevanoff24@gmail.com>
2015-03-26 21:44:36 -0700

    added 10 tests and went on

--- graph/two_dijkstras.rb
+++ graph/two_dijkstras.rb
@@ -1,9 +1,34 @@
 
 class Maze
 	attr_accessor :nodes, :connections
+	
 	def initialize(nodes, connections)
+		@nodes = nodes
+		@connections = connections
+	end
+
+	def initial_distances(goal)
+		distance = [nil]* nodes.length
+		distance[nodes.index(goal)] = 0
+		distance
+	end
+
+	def closest(nodes,distances)
+		nodes.min_by{ |node| distances[nodes.index(node)] || 10000}
+	end
+
+	def mark(node, distance)
+		
 	end
 
 	def solve(goal)
+		unchecked_nodes = nodes
+		distance = initial_distances(goal)
+		until unchecked_nodes.empty?
+			closest_node = closest(unchecked_nodes, distance)
+		mark(closest_node, distance)
+		unchecked_nodes -= [closest_node]
+		end
 	end
+
 end
--- graph/two_dijstras_spec.rb
+++ graph/two_dijstras_spec.rb
@@ -11,11 +11,50 @@ describe Maze do
 	}
 	let(:maze) { Maze.new(nodes, connections) }
 
+	describe "#initial_distances" do
+		specify do
+			expect(maze.initial_distances(:a)).to eq [0, nil, nil, nil] 
+		end
+		specify do
+			expect(maze.initial_distances(:d)).to eq [nil, nil, nil, 0] 
+		end
+	end
+
+	describe "#closest" do
+		specify do 
+			expect(maze.closest([:a], [0])).to eq (:a)
+		end
+		specify do 
+			expect(maze.closest([:a, :b], [1, 0])).to eq (:b)
+		end
+		specify do 
+			expect(maze.closest([:a, :b, :c], [1, 0, nil])).to eq (:b)
+		end
+	end
+
+	describe "#mark" do
+		specify do 
+			distances = [0, nil, nil, nil]
+			maze.mark(:a, distances)
+			expect(distances).to eq [0, 1, nil, nil]
+		end
+		specify do 
+			distances = [1, nil, nil, nil]
+			maze.mark(:a, distances)
+			expect(distances).to eq [1, 2, nil, nil]
+		end
+		specify do 
+			distances = [nil, nil, nil, 0]
+			maze.mark(:d, distances)
+			expect(distances).to eq [nil, 1, 1, 0]
+		end
+	end
+
 	describe "#solve" do 
-		it "labels the distance to a goal" do
+		xit "labels the distance to a goal" do
 			expect(maze.solve(:d)).to eq [2,1,1,0]
 		end
-		it "labels the distance to a goal" do
+		xit "labels the distance to a goal" do
 			expect(maze.solve(:a)).to eq [0,1,2,2]
 		end
 	end
