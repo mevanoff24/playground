8bfac798ed01b93dec415c53179dc611edc93b38
Matt Evanoff <mevanoff24@gmail.com>
2015-03-26 19:48:47 -0700

    my map

---
+++ enumerable/map.rb
@@ -0,0 +1,16 @@
+
+
+class Array
+	def my_map
+		result = []
+		index = 0
+		while self[index]
+			result << (yield self[index])
+			index += 1
+		end
+		result
+	end
+end
+
+
+p [1,2,3].my_map{|n| n * n }
