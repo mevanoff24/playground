9d6fb082b25d40d8bcaa4f8aeea2ce1a38903a23
Matt Evanoff <mevanoff24@gmail.com>
2015-03-28 12:40:41 -0700

    string concat

---
+++ the_req/string_concat.rb
@@ -0,0 +1,15 @@
+# String concatenation in Ruby
+# What will original and copy evaluate to in each of the following examples? 
+# Explain why there is a difference.
+
+# original = "hello" 
+# copy = original       "hello"
+# copy << " there"  
+# puts copy             "hello there"
+# puts original         "hello there"
+# original = "hello"    
+# copy = original
+# copy += " there"    
+# puts copy             "hello there"
+# puts original         "hello"
+
