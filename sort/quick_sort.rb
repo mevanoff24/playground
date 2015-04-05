a21b661b14d9cafd7776ec62bc6e99795c806c01
Matt Evanoff <mevanoff24@gmail.com>
2015-04-05 13:22:15 -0700

    quick

--- sort/quick_sort.rb
+++ sort/quick_sort.rb
@@ -35,15 +35,15 @@ class QuickSort
   end
  
   def self.partition(array, left, right)
-    pivot = array[right]
-    index = left -1
-    for number in left..right -1
+    pivot = array[left]
+    index = left +1
+    for number in left+1..right -1
       if array[number] <= pivot
         index += 1
         array[index], array[number] = array[number], array[index]
       end
     end
-    array[index+1], array[right] = array[right], array[index+1]
+    array[index+1], array[left] = array[left], array[index+1]
     index +1
   end
  
@@ -78,8 +78,8 @@ p quicksort([4,6,2,8,1])
 # recursively sort 1st part
 # recursively sort 2nd part 
 
-swap left most index with j for first
-to set pivit swap pivot with right most index 
+# swap left most index with j for first
+# to set pivit swap pivot with right most index 
 
 
 
--- stanford/quicksort.rb
+++ stanford/quicksort.rb
@@ -9,3 +9,30 @@
 # 		i +=1
 # swap A[left] and A[i-1]
 
+
+def quick(array, left=0, right=array.length-1)
+	if left < right
+		pivot_index = partition(array, left, right)
+		quick(array, left, pivot_index-1)
+		quick(array, pivot_index+1, right)
+	end
+	array
+end
+
+def partition(array, left, right)
+	pivot = array[right]
+	index = left -1
+	for number in left..right -1
+		if array[number] <= pivot
+			index +=1
+			array[index], array[number] = array[number], array[index]
+		end
+	end
+	array[index+1], array[right] = array[right], array[index+1]
+	index +1
+end
+
+p quick([34,24,3,54])
+
+
+
