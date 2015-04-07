63dcb040bed4d854f1f2b61857447c3a0a04d875
Matt Evanoff <mevanoff24@gmail.com>
2015-04-06 19:53:31 -0700

    quicksrt

--- sort/test.rb
+++ sort/test.rb
@@ -1,33 +1,23 @@
 
-def merge_and_count(array)
-	if array.length <= 1
-		return [0, array]
+def quick(array, left=0, right=array.length-1)
+	if left < right
+		pivot_index = quicksort(array, left, right)
+		quick(array, pivot_index+1, right)
+		quick(array, left, pivot_index-1)
 	end
-	middle_index = array.length/2
-	left = merge_and_count(array[0..middle_index-1])
-	right = merge_and_count(array[middle_index..array.length])
-	merge(left[1], right[1])
+	array
 end
 
-def merge(left, right)
-	output = []
-	inversions = 0
-	left_index, right_index = 0,0
-	while left.length > left_index && right.length > right_index
-		if left[left_index] < right[right_index]
-			output << left[left_index]
-			left_index += 1
-		else
-			output << right[right_index]
-			right_index += 1
-			inversions += left.length - left_index
-		end 
+def quicksort(array, left, right)
+	pivot = array[right]
+	index = left -1
+	for number in left..right-1
+		if array[number] <= pivot
+			index +=1
+			array[number], array[index] = array[index], array[number]
+		end
 	end
-	if left_index < left.length
-		output += left[left_index..left.length]
-	else
-		output += right[right_index..right.length]
-	end
-	return [inversions, output]
+	array[right], array[index+1] = array[index+1], array[right]
+	index +1
 end
-p merge_and_count([2,5,2,5,7,2,8,5,9,6,3,2,4,6,7])
+p quick([2,5,2,5,7,2,8,5,9,6,3,2,4,6,7])
