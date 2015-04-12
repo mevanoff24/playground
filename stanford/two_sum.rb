1e96c9ddd2c7dfa774ea5e8e8cba9c45e3d0f632
Matt Evanoff <mevanoff24@gmail.com>
2015-04-12 10:39:46 -0700

    sub

--- stanford/two_sum.rb
+++ stanford/two_sum.rb
@@ -3,7 +3,7 @@ def two_sum(array, target_sum)
 	result = []
 	hash = Hash[array.map {|i|[i,0]}]
 	for number in array
-		result << target_sum - number if hash[target_sum-number]
+		result << target_sum - number if hash[target_sum - number]
 	end
 	result
 end
