04a5f1325829d290e8142ca7e5035c17442f9878
Matt Evanoff <mevanoff24@gmail.com>
2015-04-12 09:28:53 -0700

    better two sum my way

--- stanford/two_sum.rb
+++ stanford/two_sum.rb
@@ -1,11 +1,11 @@
 
 def two_sum(array, target_sum)
- result = {}
- hash = Hash[array.map {|i| [i,i]}]
- for number in array
- 	result[number] = target_sum - number if hash[target_sum - number] # && !result[target_sum-number]
- end
- result
+	result = []
+	hash = Hash[array.map {|i|[i,0]}]
+	for number in array
+		result << target_sum - number if hash[target_sum-number]
+	end
+	result
 end
 
 p two_sum([3,6,1,9,8,3], 17)
