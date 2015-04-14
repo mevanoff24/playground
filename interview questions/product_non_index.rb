1d737dede76d04ca678a94af4931fb9f5f7a54ad
Matt Evanoff <mevanoff24@gmail.com>
2015-04-13 21:35:46 -0700

    product array

---
+++ interview questions/product_non_index.rb
@@ -0,0 +1,32 @@
+  def get_products_of_all_ints_except_at_index(int_array)
+
+    # we make an array with the length of the input array to
+    # hold our products
+    products_of_all_ints_except_at_index = [1] * int_array.length
+
+    # for each integer, we find the product of all the integers
+    # before it, storing the total product so far each time
+    product = 1
+    i = 0
+    while i < int_array.length
+       products_of_all_ints_except_at_index[i] = product
+       product *= int_array[i]
+       i += 1
+    end
+    
+    # for each integer, we find the product of all the integers
+    # after it. since each index in products already has the
+    # product of all the integers before it, now we're storing
+    # the total product of all other integers
+    product = 1
+    i = int_array.length - 1
+    while i >= 0
+      products_of_all_ints_except_at_index[i] *= product
+      product *= int_array[i]
+      i -= 1
+     end
+
+    return products_of_all_ints_except_at_index
+  end
+
+  p get_products_of_all_ints_except_at_index([3,4,5,6])
