475ccf1fad5f0d13830eb4da97346aac5a519304
Matt Evanoff <mevanoff24@gmail.com>
2015-04-21 21:37:21 -0700

    reverse words

--- common/reverse_string.rb
+++ common/reverse_string.rb
@@ -2,18 +2,18 @@
 
 def reverse_string(string)
 	word = ''
-	chars = string.chars
-	chars.length.times { word << chars.pop }
-	word	
+	char = string.chars
+	char.length.times {word << char.pop}
+	word
 end
 
-p reverse_string("Hello World")
+p reverse_string("Hello World") == "dlroW olleH"
 
 def reverse_mult_string(string)
 	string = string.split(' ')
 	(string.length/2).times do |i|
 		string[i], string[-1-i] = string[-1-i], string[i]
 	end
-	string
+	string.join(' ')
 end
- p reverse_mult_string("Hello World")
+ p reverse_mult_string("Hello World") == "World Hello"
