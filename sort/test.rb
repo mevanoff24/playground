b4f1c6ee17adce0ff4d8934f3180a20efc2739ab
Matt Evanoff <mevanoff24@gmail.com>
2015-04-28 10:40:47 -0700

    mastered quick and merge

--- sort/test.rb
+++ sort/test.rb
@@ -1,35 +1,24 @@
 
-def merge_sort(array)
-	if array.length <= 1
-		return [0, array]
+def quicksort(array, left=0, right=array.length-1)
+	if left < right
+		pivot_index = partition(array, left, right)
+		quicksort(array, left, pivot_index-1)
+		quicksort(array, pivot_index+1, right)
 	end
-		middle_index = array.length/2
-		left = merge_sort(array[0..middle_index-1])
-		right = merge_sort(array[middle_index..array.length])
-		merge(left[1], right[1])
+	array
 end
 
-def merge(left, right)
-	output = []
-	inversions = 0
-	left_index,right_index = 0,0
-	while left_index < left.length && right_index < right.length
-		if left[left_index] < right[right_index]
-			output << left[left_index]
-			left_index +=1
-		else
-			output << right[right_index]
-			right_index +=1
-			inversions += left.length - left_index
+def partition(array, left, right)
+	pivot = array[right]
+	index = left-1
+	for number in left..right-1
+		if array[number] <= pivot
+			index += 1
+			array[number], array[index] = array[index], array[number]
 		end
 	end
-
-	if left_index < left.length
-		output += left[left_index..left.length]
-	else
-		output += right[right_index..right.length]
-	end
-	return [inversions, output]
+	array[index+1], array[right] = array[right], array[index+1]
+	index + 1
 end
 
-p merge_sort([2,4,1,9,5,3,7,2,9,2])
+p quicksort([2,4,1,9,5,3,7,2,9,2])
